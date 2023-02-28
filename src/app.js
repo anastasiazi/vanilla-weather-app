84934c87ee1a60f23bef05a1627cf35470349c35
anastasiazi <anastasiiiazi@gmail.com>
2023-02-28 12:28:16 -0500

    feature: format function integrated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    };
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = date.getDay();
+    return `${days[day]}  ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response);
     let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +20,14 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 const apiKey = "4904e8e60b2d25ac4bf6450fbbt3bo36";
